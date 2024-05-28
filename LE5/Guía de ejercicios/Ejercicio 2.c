7a5944c563af87dcbb82d3bcc6486111a5c8bf26
Vicente-RR <vicente.rodriguez.rapetti@gmail.com>
2024-05-28 16:56:18 -0300

    Empecé ejercicio 2

---
+++ LE5/Guía de ejercicios/Ejercicio 2.c
@@ -0,0 +1,39 @@
+/*
+1) a vale 5
+2) a vale 5
+
+CÓDIGO PARA COMPROBAR:
+*/
+int a=5;
+float b=13.546;
+char c='A';
+char d='a';
+
+int main()
+{
+  printf ("a vale %d \n", a);
+  printf ("a vale %o \n", a);
+  printf ("a vale %X \n", a);
+  printf ("a vale %f \n", a);
+  printf ("a vale %c \n", a);
+  printf ("b vale %d \n", b);
+  printf ("b vale %5.2d \n", b);
+  printf ("b vale %f \n", b);
+  printf ("b vale %.1f \n", b);
+  printf ("b vale %.2f \n", b);
+  printf ("b vale %6.4f \n", b);
+  printf ("b vale %6.1f \n", b);
+  printf ("b vale %c \n", b);
+  printf ("b vale %o \n", b);
+  printf ("b vale %X \n", b);
+  printf ("c vale %d \n", c);
+  printf ("c vale %f \n", c);
+  printf ("c vale %c \n", c);
+  printf ("c vale %o \n", c);
+  printf ("c vale %X \n", c);
+  printf ("d vale %d \n", d);
+  printf ("d vale %f \n", d);
+  printf ("d vale %c \n", d);
+  printf ("d vale %o \n", d);
+  printf ("d vale %X \n", d);
+}
