5014f55f334017d5037fdd8c38c7fc2def863630
Vicente-RR <vicente.rodriguez.rapetti@gmail.com>
2024-05-29 15:21:39 -0300

    Avance ejercicio 2

--- LE5/Guía de ejercicios/Ejercicio 2.c
+++ LE5/Guía de ejercicios/Ejercicio 2.c
@@ -1,9 +1,37 @@
 /*
 1) a vale 5
 2) a vale 5
-
+3) a vale 5
+4) a vale 0.0
+5) a vale ♣ //(depende del sistema)
+//
+13,546(10) = 1101,10001011110001101010(2) = 1,10110001011110001101010*2^3 = 1,10110001011110001101010*2^(10000010-01111111)
+Como está codificado en IEEE 754, el número es 01000001010110001011110001101010 -> 1096334442(10)
+//
+6) b vale 
+7) b vale 
+8) b vale 
+9) b vale 
+10) b vale 
+11) b vale 
+12) b vale 
+13) b vale 
+14) b vale 
+15) b vale 
+16) c vale 
+17) c vale 
+18) c vale 
+19) c vale 
+20) c vale 
+21) d vale 
+22) d vale 
+23) d vale 
+24) d vale 
+25) d vale 
 CÓDIGO PARA COMPROBAR:
 */
+#include <stdio.h>
+
 int a=5;
 float b=13.546;
 char c='A';
