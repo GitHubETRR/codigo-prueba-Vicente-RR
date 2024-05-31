84cd6756803d5d3ce9e4a1ff343adba8c5f81b6e
Vicente-RR <vicente.rodriguez.rapetti@gmail.com>
2024-05-31 14:50:19 -0300

    Empecé ejercicio 3

---
+++ LE5/Guía de ejercicios/Ejercicio 3.c
@@ -0,0 +1,22 @@
+/*
+1) Se almacena un número entero enviado por el usuario. Si se envía un número no entero, se almacena la parte entera.
+2) 
+*/
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  float b;
+  char c;
+   
+  scanf ("%d", &a);
+  scanf ("%f", &a);
+  scanf ("%c", &a);
+  scanf ("%d", &b);
+  scanf ("%f", &b);
+  scanf ("%c", &b);
+  scanf ("%d", &c);
+  scanf ("%f", &c);
+  scanf ("%f", &c);
+}
