b957708793a53c8be38f903b74de27a72625e948
Vicente-RR <vicente.rodriguez.rapetti@gmail.com>
2024-06-28 10:47:47 -0300

    Hice el ejercicio 10

---
+++ LE5/Guía de ejercicios/Ejercicio 10.c
@@ -0,0 +1,21 @@
+//Programa 1 (falta una parte del código)
+#include <stdio.h>
+
+int main(){
+    int n,total;
+    float promedio;
+    n=0; //Acá debería preguntar la cantidad de números a sumar y realizar la suma, guardándola en el total
+    promedio=total/n; //En esta operación no hay ni un total ni la cantidad de números (siempre es 0). Además, la división entre dos enteros no da un float, y esto se puede solucionar poniendo (float) para pasar momentáneamente los int a float
+    printf("Prom=%f\n",promedio);
+    return 0;
+}
+//Programa 2 (todo mal)
+#include <stdio.h>
+
+int main(void){ //Dejamos main() vacío
+    integer x; //int
+    real y; //float/double
+    scanf ("%d",y); //Se intenta leer la variable real (que debería ser float o double) como entero. También falta el &
+    printf("%f",x) //Se intenta printear la variable entera como float. También falta el ;
+    return 0;
+}
